0803ba0f7be2a3e91dff5bef5c19c6e19fcc9ab6
Blacktacky <144636461+Blacktacky@users.noreply.github.com>
2023-09-16 18:37:51 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,14 +1,15 @@
 variable"subscription_id"{
-
+type=string
 }
 variable "client_id"{
-
+type=sting
 }
 variable "client_secret"{
-
-
+type=string
 }
 variable "tenant_id"{
-
-
+type=string
+}
+variable"prefix"{
+type=string
 }
