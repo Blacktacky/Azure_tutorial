5a1b1d84a927a278cb83f906a3b88de6ae2ac804
Blacktacky <144636461+Blacktacky@users.noreply.github.com>
2023-09-16 19:37:53 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "azurerm_resource_group""azure_tutorial"{
     location="Canada Central"
 }
 resource "azurerm_storage_account" "awp" {
-  name                     = "${var.prefix}storageaccountname-${var.env}"
+  name                     = "${var.prefix}storage-${var.env}"
   resource_group_name      = azurerm_resource_group.azure_tutorial.name
   location                 = azurerm_resource_group.azure_tutorial.location
   account_tier             = var.account_tier
