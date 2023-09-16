102954802d5795a0f5a36ef38d8d0efaa99a3568
Blacktacky <144636461+Blacktacky@users.noreply.github.com>
2023-09-16 18:58:30 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-reource "azurerm_resource_group""azure_tutorial"{
-    name="azure_tutorial_Resource_Group"
+resource "azurerm_resource_group""azure_tutorial"{
+    name="Prefix_Resource_Group"
     location="Canada Central"
 }
