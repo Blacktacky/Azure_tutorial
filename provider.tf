2a3a76d470e4b739ad01f2e1fd57e069821ac415
Blacktacky <144636461+Blacktacky@users.noreply.github.com>
2023-09-16 19:01:34 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform{
   required_providers{
     azurerm={
       source="hashicorp/arurerm"
-      version=">=3.70.0"#this version is for azurerm, NOT terraform version
+      version=">=3.50.0"#this version is for azurerm, NOT terraform version
     }
   }
   required_version=">=1.5.0"#this version is for Terraform Version, NOT azurerm
