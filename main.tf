5a6f3ac8582beecb8d45849824f36b2884841d7a
Blacktacky <144636461+Blacktacky@users.noreply.github.com>
2023-09-16 19:29:03 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ resource "azurerm_resource_group""azure_tutorial"{
 }
 resource "azurerm_storage_account" "awp" {
   name                     = "${var.prefix}storageaccountname-${var.env}"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  resource_group_name      = azurerm_resource_group.azure_tutorial.name
+  location                 = azurerm_resource_group.azure_tutorial.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
