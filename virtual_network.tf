a34908b80394948170e2475b85acac0167f219bc
Blacktacky <144636461+Blacktacky@users.noreply.github.com>
2023-09-25 21:47:45 -0400

    Update virtual_network.tf

--- virtual_network.tf
+++ virtual_network.tf
@@ -7,29 +7,29 @@ resource "azurerm_virtual_network" "vn" {
 
 resource "azurerm_subnet" "frontend" {
   name                 = "frontend"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = azurerm_resource_group.azure_tutorial.name
+  virtual_network_name = azurerm_virtual_network.vn.name
   address_prefixes     = ["10.254.0.0/24"]
 }
 
 resource "azurerm_subnet" "backend" {
   name                 = "backend"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = azurerm_resource_group.azure_tutorial.name
+  virtual_network_name = azurerm_virtual_network.vn.name
   address_prefixes     = ["10.254.2.0/24"]
 }
 
 resource "azurerm_public_ip" "example" {
   name                = "example-pip"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.azure_tutorial.name
+  location            = azurerm_resource_group.azure_tutorial.location
   allocation_method   = "Dynamic"
 }
 
 resource "azurerm_application_gateway" "network" {
   name                = "example-appgateway"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.azure_tutorial.name
+  location            = azurerm_resource_group.azure_tutorial.location
 
   sku {
     name     = "Standard_Small"
