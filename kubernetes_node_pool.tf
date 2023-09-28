2134c72c0d7b40f1fd9562c769ea29f84458c844
Blacktacky <144636461+Blacktacky@users.noreply.github.com>
2023-09-28 16:46:43 -0400

    Update kubernetes_node_pool.tf

--- kubernetes_node_pool.tf
+++ kubernetes_node_pool.tf
@@ -1,1 +1,6 @@
-
+resource "azurerm_kubernetes_cluster_node_pool" "kube_pool" {
+  name                  = "internal"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.kube1.id
+  vm_size               = "Standard_DS2_v2"
+  node_count            = 1
+}
