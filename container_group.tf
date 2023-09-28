bae6b4ba0d4c917f008d640fe9a58d5946072d34
Blacktacky <144636461+Blacktacky@users.noreply.github.com>
2023-09-28 17:06:41 -0400

    Update container_group.tf

--- container_group.tf
+++ container_group.tf
@@ -2,7 +2,7 @@ resource "azurerm_container_group" "acg" {
   name                = "example-continst"
   location            = azurerm_resource_group.azure_tutorial.location
   resource_group_name = azurerm_resource_group.azure_tutorial.name
-  ip_address_type     = "public"
+  ip_address_type     = "Public"
   dns_name_label      = "aci-label"
   os_type             = "Linux"
 
