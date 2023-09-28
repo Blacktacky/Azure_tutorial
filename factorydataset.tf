be51800926c327d2308d7872808c7453587c8604
Blacktacky <144636461+Blacktacky@users.noreply.github.com>
2023-09-28 17:05:58 -0400

    Update factorydataset.tf

--- factorydataset.tf
+++ factorydataset.tf
@@ -1,13 +1,11 @@
 resource "azurerm_data_factory_linked_service_mysql" "dflsm" {
-  name                = "example"
-  resource_group_name = azurerm_resource_group.azure_tutorial.name
-  data_factory_name   = azurerm_data_factory.factory1.name
-  connection_string   = "Server=test;Port=3306;Database=test;User=test;SSLMode=1;UseSystemTrustStore=0;Password=test"
+  name              = "example"
+  data_factory_id   = azurerm_data_factory.factory1.id
+  connection_string = "Server=test;Port=3306;Database=test;User=test;SSLMode=1;UseSystemTrustStore=0;Password=test"
 }
 
 resource "azurerm_data_factory_dataset_mysql" "dfdm" {
   name                = "example"
-  resource_group_name = azurerm_resource_group.azure_tutorial.name
-  data_factory_name   = azurerm_data_factory.factory1.name
-  linked_service_name = azurerm_data_factory_linked_service_mysql.dflsm.name
+  data_factory_id     = azurerm_data_factory.factory1.id
+  linked_service_name = azurerm_data_factory_linked_service_mysql.example.name
 }
