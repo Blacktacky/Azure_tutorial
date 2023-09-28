21cda6d9a364cf82f067dcebe9a46294404d40a3
Blacktacky <144636461+Blacktacky@users.noreply.github.com>
2023-09-28 16:36:42 -0400

    Update availability.tf

--- availability.tf
+++ availability.tf
@@ -1,7 +1,7 @@
 resource "azurerm_availability_set" "ava" {
   name                = "acceptanceTestAvailabilitySet1"
   location            = azurerm_resource_group.azure_tutorial.location
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = azurerm_resource_group.azure_tutorial.name
 
   tags = {
     environment = "Production"
