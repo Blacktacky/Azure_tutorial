60a1f44f25196386a6eee5d5462a3a3a03207045
Blacktacky <144636461+Blacktacky@users.noreply.github.com>
2023-09-28 16:51:53 -0400

    Update datafactory.tf

--- datafactory.tf
+++ datafactory.tf
@@ -1,1 +1,5 @@
-
+resource "azurerm_data_factory" "factory1" {
+  name                = "example"
+  location            = azurerm_resource_group.azure_tutorial.location
+  resource_group_name = azurerm_resource_group.azure_tutorial.name
+}
