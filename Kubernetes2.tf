f119193881ceb970dc3196ba4c84658b56eed1a1
Blacktacky <144636461+Blacktacky@users.noreply.github.com>
2023-09-28 21:07:04 -0400

    Update Kubernetes2.tf

--- Kubernetes2.tf
+++ Kubernetes2.tf
@@ -1,1 +1,29 @@
+resource "azurerm_kubernetes_cluster" "kub2" {
+  name                = "example-aks1"
+  location            = azurerm_resource_group.azure_tutorial.location
+  resource_group_name = azurerm_resource_group.azure_tutorial.name
+  dns_prefix          = "exampleaks1"
 
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  service_principal {
+    client_id     = "00000000-0000-0000-0000-000000000000"
+    client_secret = "00000000000000000000000000000000"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
+
+output "client_certificate" {
+  value = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.example.kube_config_raw
+}
