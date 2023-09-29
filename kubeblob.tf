6a8293a71f930bfa61158b4701f3d61748d5eda9
Blacktacky <144636461+Blacktacky@users.noreply.github.com>
2023-09-28 22:11:42 -0400

    Update kubeblob.tf

--- kubeblob.tf
+++ kubeblob.tf
@@ -1,1 +1,26 @@
+locals{
+  csgo_weapons=["ak47","m4","aug","bizon"]
+}
 
+resource "azurerm_storage_account" "kubeblob" {
+  for_each                 = {for cluster in local.csgo_weapons:cluster=>cluster}
+  name                     = "${var.prefix}cluster-${each.key}"
+  resource_group_name      = azurerm_resource_group.azure_tutorial.name
+  location                 = azurerm_resource_group.azure_tutorial.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "kubeblobcon" {
+  name                  = "content"
+  storage_account_name  = azurerm_storage_account.kubeblob.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "example" {
+  name                   = "my-awesome-content.zip"
+  storage_account_name   = azurerm_storage_account.kubeblob.name
+  storage_container_name = azurerm_storage_container.kubeblobcon.name
+  type                   = "Block"
+  source                 = "some-local-file.zip"
+}
