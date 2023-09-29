a59610a78df72773c555ae45283f3b56823fd74f
Blacktacky <144636461+Blacktacky@users.noreply.github.com>
2023-09-28 21:45:22 -0400

    Update Kubernetes2.tf

--- Kubernetes2.tf
+++ Kubernetes2.tf
@@ -1,10 +1,9 @@
 locals{
   cluster_names["k8batcha06","k9batcha06","k10batcha06","k11batcha06","k12batcha06"]
-
-
+}
 resource "azurerm_kubernetes_cluster" "kub2" {
-  for_each            = {for cluster in local.cluster_names:cluster}
-  name                = "example-aks1"
+  for_each            = {for cluster in local.cluster_names:cluster=>cluster}
+  name                = "${var.prefix}cluster-${each.key}"
   location            = azurerm_resource_group.azure_tutorial.location
   resource_group_name = azurerm_resource_group.azure_tutorial.name
   dns_prefix          = "exampleaks1"
@@ -14,6 +13,8 @@ resource "azurerm_kubernetes_cluster" "kub2" {
     node_count = 1
     vm_size    = "Standard_D2_v2"
   }
+  identity {
+    type = "SystemAssigned"
 
   tags = {
     Environment = "Production"
@@ -21,9 +22,9 @@ resource "azurerm_kubernetes_cluster" "kub2" {
 }
 
 output "client_certificate" {
-  value = azurerm_kubernetes_cluster.kub2.kube_config.0.client_certificate
+  value = [for cluster in azurem_kubernetes_cluster.kub2:cluster.kube_config.0.client_certificate]
 }
 
 output "kube_config" {
-  value = azurerm_kubernetes_cluster.kub2.kube_config_raw
+  value = [for cluster in  azurerm_kubernetes_cluster.kub2:cluster.kube_config_raw]
 }
