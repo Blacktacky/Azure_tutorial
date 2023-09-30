4fee08cf8777f2c05d3b2e4c091801c19e27df0a
Blacktacky <144636461+Blacktacky@users.noreply.github.com>
2023-09-30 18:35:58 -0400

    Update kube_node_pool2.tf

--- kube_node_pool2.tf
+++ kube_node_pool2.tf
@@ -1,1 +1,7 @@
-
+resource "azurerm_kubernetes_cluster_node_pool" "kube_pool2" {
+  for_each              =azurerm_kubernetes_cluster.kub2
+  name                  = "${each.key}"
+  kubernetes_cluster_id = each.value.id
+  vm_size               = "Standard_DS2_v2"
+  node_count            = 1
+}
