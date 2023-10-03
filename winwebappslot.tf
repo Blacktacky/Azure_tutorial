b04be6f75c783cef8c9f03a0fc8569c1d8781432
Blacktacky <144636461+Blacktacky@users.noreply.github.com>
2023-10-03 17:26:13 -0400

    Update winwebappslot.tf

--- winwebappslot.tf
+++ winwebappslot.tf
@@ -1,1 +1,10 @@
+provider "azurerm" {
+  features {}
+}
 
+resource "azurerm_windows_web_app_slot" "wwas" {
+  name           = "example-slot"
+  app_service_id = azurerm_windows_web_app.awwa.id
+
+  site_config {}
+}
