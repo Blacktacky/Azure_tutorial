98b79836f4a24c089c1e15191562d5ffcf4dc5eb
Blacktacky <144636461+Blacktacky@users.noreply.github.com>
2023-10-03 17:54:23 -0400

    Update linwebappslot.tf

--- linwebappslot.tf
+++ linwebappslot.tf
@@ -1,5 +1,6 @@
 resource "azurerm_linux_web_app_slot" "alwas" {
-  name           = "example-slot"
+  for_each       =azurerm_linux_web_app.alwa
+  name           = "${each.key}"
   app_service_id = azurerm_linux_web_app.alwa.id
 
   site_config {}
