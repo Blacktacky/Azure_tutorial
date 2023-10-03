ccf81b7be9c0a6651e6fb55b6729a7cd6448ba0e
Blacktacky <144636461+Blacktacky@users.noreply.github.com>
2023-10-03 17:23:51 -0400

    Update linwebappslot.tf

--- linwebappslot.tf
+++ linwebappslot.tf
@@ -1,1 +1,10 @@
+provider "azurerm" {
+  features {}
+}
 
+resource "azurerm_linux_web_app_slot" "alwas" {
+  name           = "example-slot"
+  app_service_id = azurerm_linux_web_app.alwa.id
+
+  site_config {}
+}
