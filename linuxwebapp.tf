9d2c4cee9129978713a6eac39056c4f021ef4897
Blacktacky <144636461+Blacktacky@users.noreply.github.com>
2023-10-03 17:14:46 -0400

    Update linuxwebapp.tf

--- linuxwebapp.tf
+++ linuxwebapp.tf
@@ -1,1 +1,20 @@
+provider "azurerm" {
+  features {}
+}
 
+resource "azurerm_service_plan" "lasp" {
+  name                = "example"
+  resource_group_name = azurerm_resource_group.azure_tutorial.name
+  location            = azurerm_resource_group.azure_tutorial.location
+  os_type             = "Linux"
+  sku_name            = "P1v2"
+}
+
+resource "azurerm_linux_web_app" "alwa" {
+  name                = "example"
+  resource_group_name = azurerm_resource_group.azure_tutorial.name
+  location            = azurerm_service_plan.lasp.location
+  service_plan_id     = azurerm_service_plan.example.id
+
+  site_config {}
+}
