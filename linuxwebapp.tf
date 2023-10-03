ed8edcdd86c8f36059f0b0b21b4257c16d835980
Blacktacky <144636461+Blacktacky@users.noreply.github.com>
2023-10-03 17:49:26 -0400

    Update linuxwebapp.tf

--- linuxwebapp.tf
+++ linuxwebapp.tf
@@ -1,3 +1,7 @@
+locals{
+  rainbow_ops=["bandit","ace","thermite","sledge","buck","smoke","mute","iq","zofia","ela","jaeger"]
+}
+
 resource "azurerm_service_plan" "lasp" {
   name                = "example"
   resource_group_name = azurerm_resource_group.azure_tutorial.name
@@ -7,7 +11,8 @@ resource "azurerm_service_plan" "lasp" {
 }
 
 resource "azurerm_linux_web_app" "alwa" {
-  name                = "example"
+  for_each            ={for rainbow in local.rainbow_ops:rainbow=>rainbow}
+  name                = "${var.prefix}rainbow-${each.key}"
   resource_group_name = azurerm_resource_group.azure_tutorial.name
   location            = azurerm_service_plan.lasp.location
   service_plan_id     = azurerm_service_plan.lasp.id
