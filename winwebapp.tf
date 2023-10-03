591ff4416e45ffae8f0ee88863362b66797f88ef
Blacktacky <144636461+Blacktacky@users.noreply.github.com>
2023-10-03 17:16:50 -0400

    Update winwebapp.tf

--- winwebapp.tf
+++ winwebapp.tf
@@ -1,1 +1,20 @@
+provider "azurerm" {
+  features {}
+}
 
+resource "azurerm_service_plan" "wasp" {
+  name                = "example"
+  resource_group_name = azurerm_resource_group.azure_tutorial.name
+  location            = azurerm_resource_group.azure_tutorial.location
+  sku_name            = "P1v2"
+  os_type             = "Windows"
+}
+
+resource "azurerm_windows_web_app" "awwa" {
+  name                = "example"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_service_plan.wasp.location
+  service_plan_id     = azurerm_service_plan.example.id
+
+  site_config {}
+}
