a3849036fbb83dc96a5f93bf87af32f319dbd049
Blacktacky <144636461+Blacktacky@users.noreply.github.com>
2023-10-03 22:22:32 -0400

    Update lb.tf

--- lb.tf
+++ lb.tf
@@ -1,1 +1,17 @@
+resource "azurerm_public_ip" "api" {
+  name                = "PublicIPForLB"
+  location            = azurerm_resource_group.azure_tutorial.location
+  resource_group_name = azurerm_resource_group.azure_tutorial.name
+  allocation_method   = "Static"
+}
 
+resource "azurerm_lb" "alb" {
+  name                = "TestLoadBalancer"
+  location            = azurerm_resource_group.azure_tutorial.location
+  resource_group_name = azurerm_resource_group.azure_tutorial.name
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.api.id
+  }
+}
