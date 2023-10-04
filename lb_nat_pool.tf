90234a5493557cab9c47a817f8350cd452d83c5f
Blacktacky <144636461+Blacktacky@users.noreply.github.com>
2023-10-03 22:27:31 -0400

    Update lb_nat_pool.tf

--- lb_nat_pool.tf
+++ lb_nat_pool.tf
@@ -1,1 +1,10 @@
-
+resource "azurerm_lb_nat_pool" "albnp" {
+  resource_group_name            = azurerm_resource_group.azure_tutorial.name
+  loadbalancer_id                = azurerm_lb.alb.id
+  name                           = "SampleApplicationPool"
+  protocol                       = "Tcp"
+  frontend_port_start            = 80
+  frontend_port_end              = 81
+  backend_port                   = 8080
+  frontend_ip_configuration_name = "PublicIPAddress"
+}
