d8ba71df57a0546608ea7467ff37141e9e638054
Blacktacky <144636461+Blacktacky@users.noreply.github.com>
2023-10-03 21:37:31 -0400

    Update virtual_network.tf

--- virtual_network.tf
+++ virtual_network.tf
@@ -97,7 +97,6 @@ resource "azurerm_application_gateway" "network" {
   }
 
   http_listener {
-    for_each                       =azurerm_web_application_firewall_policy.wafp
     name                           = "${each.key}"
     frontend_ip_configuration_name = local.frontend_ip_configuration_name
     frontend_port_name             = local.frontend_port_name
