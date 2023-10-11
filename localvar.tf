9fee979bdb3462dffd93f81c13ae6593f98dc81b
Blacktacky <144636461+Blacktacky@users.noreply.github.com>
2023-10-10 21:32:34 -0400

    Update localvar.tf

--- localvar.tf
+++ localvar.tf
@@ -1,7 +1,7 @@
 sql_server=[for f in fileset("${path.module}/yaml", "[^_]*.yaml") : yamldecode(file("${path.module}/yaml/${f}"))]
   sql_server_list = flatten([
   for app in local.sql_server : [
-      for sqlservers in try(app.listoflinuxapp, []) :{
+      for sqlservers in try(app.listofsqlserver, []) :{
         name=sqlservers.name
         version:sqlserver.version
               }
