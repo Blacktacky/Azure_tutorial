e271957c847a2cc70f43a6121b158b92455de970
Blacktacky <144636461+Blacktacky@users.noreply.github.com>
2023-10-10 21:05:36 -0400

    Update sql_database.tf

--- sql_database.tf
+++ sql_database.tf
@@ -1,16 +1,3 @@
-resource "azurerm_sql_server" "example" {
-  name                         = "myexamplesqlserver"
-  resource_group_name          = azurerm_resource_group.example.name
-  location                     = azurerm_resource_group.example.location
-  version                      = "12.0"
-  administrator_login          = "4dm1n157r470r"
-  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
-
-  tags = {
-    environment = "production"
-  }
-}
-
 resource "azurerm_storage_account" "example" {
   name                     = "examplesa"
   resource_group_name      = azurerm_resource_group.example.name
