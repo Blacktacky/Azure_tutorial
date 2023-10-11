5fec9c6213bc8a8e36b1dead7509c65a72a8cb0d
Blacktacky <144636461+Blacktacky@users.noreply.github.com>
2023-10-10 21:17:04 -0400

    Update sqlserver.tf

--- sqlserver.tf
+++ sqlserver.tf
@@ -3,10 +3,13 @@ resource "azurerm_sql_server" "example" {
   resource_group_name          = azurerm_resource_group.azure_tutorial.name
   location                     = azurerm_resource_group.azure_tutorial.location
   version                      = "12.0"
-  administrator_login          = "4dm1n157r470r"
-  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+  administrator_login          = var.administrator_login
+  administrator_login_password = var.administrator_login_password
 
   tags = {
     environment = "production"
   }
 }
+
+4dm1n157r470r
+4-v3ry-53cr37-p455w0rd
