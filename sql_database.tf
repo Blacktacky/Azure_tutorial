f72deec9224da15b0e5f41135b4ace7cbd9156ac
Blacktacky <144636461+Blacktacky@users.noreply.github.com>
2023-10-10 21:48:48 -0400

    Update sql_database.tf

--- sql_database.tf
+++ sql_database.tf
@@ -1,16 +1,16 @@
-resource "azurerm_storage_account" "example" {
+resource "azurerm_storage_account" "asqa" {
   name                     = "examplesa"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  resource_group_name      = azurerm_resource_group.azure_tutorial.name
+  location                 = azurerm_resource_group.azure_tutorial.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-resource "azurerm_sql_database" "example" {
+resource "azurerm_sql_database" "asqd" {
   name                = "myexamplesqldatabase"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  server_name         = azurerm_sql_server.example.name
+  resource_group_name = azurerm_resource_group.azure_tutorial.name
+  location            = azurerm_resource_group.azure_tutorial.location
+  server_name         = azurerm_sql_server.asqs.name
 
   tags = {
     environment = "production"
